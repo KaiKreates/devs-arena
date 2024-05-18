2833bce4b2a8442518afdde42a00a012a9093185
Kaiver <krazykidkeshav@gmail.com>
2024-05-18 23:12:39 +0530

    Environment Alpha

--- Assets/Scripts/SharkController.cs
+++ Assets/Scripts/SharkController.cs
@@ -4,23 +4,54 @@ using UnityEngine;
 
 public class SharkController : MonoBehaviour
 {
-    private Vector3 targetPos;
+    private Vector3 playerPos;
     public Transform player;
-    public float runSpeed;
+
+    private float cooldown = 0;
+    public float cooldownDuration;
+    public float swimSpeed;
+    public float slowSpeed;
+
+    public LayerMask whatIsRock;
+    public LayerMask whatIsPlayer;
+
     public void Start()
     {
-        Debug.Log("Chase State");
-        targetPos = player.position;
+
     }
 
     public void Update()
     {
+        playerPos = player.position;
         //Rotate towards target position
-        Vector3 lookDir = Vector3.RotateTowards(transform.forward, targetPos - transform.position, 1 * Time.deltaTime, 0);
+        Vector3 targetPos = new Vector3(playerPos.x - transform.position.x, 0, playerPos.z - transform.position.z);
+        Vector3 lookDir = Vector3.RotateTowards(transform.forward, targetPos, 1 * Time.deltaTime, 0);
         transform.rotation = Quaternion.LookRotation(lookDir);
 
         //Move Forward
-        transform.position += transform.forward * runSpeed * Time.deltaTime;
+        transform.position += transform.forward * swimSpeed * Time.deltaTime;
 
+        float delta = 5;
+        for (int i = -3; i <= 3; i++)
+        {
+            //Wall Detection
+            var dir = Quaternion.Euler(0, i * delta, 0) * transform.forward;
+            Debug.DrawRay(transform.position, dir * 8, Color.green);
+
+            if (Physics.Raycast(transform.position, dir, 8, whatIsRock))
+            {
+                swimSpeed = slowSpeed;
+                cooldown = Time.time + cooldownDuration;
+            }
+        }
+
+        if(cooldown < Time.time)
+            //revert back to swimSpeed;
+
+        //Detect Player
+        if (Physics.Raycast(transform.position, transform.forward, 3, whatIsPlayer))
+        {
+            //Gameover
+        }
     }
 }
