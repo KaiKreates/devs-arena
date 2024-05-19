3e69d007d8fd3ca81ffba63b2f63c425ce05efd3
Kaiver <krazykidkeshav@gmail.com>
2024-05-20 01:15:08 +0530

    Level 1 and 2

--- Assets/Scripts/PlayerHandler.cs
+++ Assets/Scripts/PlayerHandler.cs
@@ -6,9 +6,16 @@ public class PlayerHandler : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+        if (other.gameObject.CompareTag("Lava"))
+        {
+            Debug.Log("Game Over");
+        }
+
         if (other.gameObject.CompareTag("Finish"))
         {
             //Level Complete Screen
+            Debug.Log("Level Complete");
         }
+
     }
 }
